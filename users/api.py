a012c6eb016e280ebab0486d0e171714d7881b11
Eldar Muminov <emuminov@z4r6p7.42lyon.fr>
2025-01-13 20:06:13 +0100

    update

--- users/api.py
+++ users/api.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 from ninja import NinjaAPI, Schema
+from ninja.files import UploadedFile
 
 from .models import Track
 from .schema import NotFoundSchema, TrackSchema
@@ -42,3 +43,31 @@ def get_track(request, track_id: int):
 def post_track(request, track: TrackSchema):
     track = Track.objects.create(**track.dict())
     return track
+
+
+@api.put("tracks/{track_id}", response={200: TrackSchema, 404: NotFoundSchema})
+def update_track(request, track_id: int, data: TrackSchema):
+    try:
+        track = Track.objects.get(pk=track_id)
+        for attribute, value in data.dict().items():
+            setattr(track, attribute, value)
+            track.save()
+        return 200, track
+    except Track.DoesNotExist:
+        return 404, {"message": "Track does not exist"}
+
+
+@api.delete("tracks/{track_id}", response={204: None, 404: NotFoundSchema})
+def delete_track(request, track_id: int):
+    try:
+        track = Track.objects.get(pk=track_id)
+        track.delete()
+        return 204, None
+    except Track.DoesNotExist:
+        return 404, {"message": "Track does not exist"}
+
+
+@api.post("/upload")
+def upload(request, file: UploadedFile):
+    data = file.read().decode()
+    return {"name": file.name}
